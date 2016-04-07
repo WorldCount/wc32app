0efbcd797ce8e128a26e6d275cbbbf24a7757c76
WorldCount <worldcount@yandex.ru>
2016-04-07 20:50:57 +0400

    Инициализация пакета

---
+++ __init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+Пакет для работы с интерфейсом
+"""
+
+__date__ = "08.03.2016"
+__author__ = "WorldCount"
+__email__ = "world.count@yandex.ru"
+__copyright__ = "Copyright 2016, Scr1pt1k.Ru"
+__python_version__ = "3"
+
+
+VERSION = '3.0.0'
