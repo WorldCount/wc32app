f2167f486d92996c78dbe068ba4240965ca1102a
WorldCount <worldcount@yandex.ru>
2016-04-17 18:29:55 +0400

    Добавлен класс системного расширения для консоли.

--- wcextends.py
+++ wcextends.py
@@ -32,3 +32,50 @@ class WCClearExtend(object):
         self.open_tags = {}
         # Закрывающие теги
         self.close_tags = {}
+
+
+# Класс: системное расширение
+class SystemExtend(WCClearExtend):
+
+    """
+    Класс системного расширения для консоли на PyQt5
+    @author WorldCount
+    @version 3
+    @date 2016/04/17
+    """
+
+    #
+    def __init__(self):
+        super(SystemExtend, self).__init__()
+        self.name = 'SystemExtend'
+        self.styles = {'system': '#756e58',
+                       'error': '#e14343',
+                       'info': '#44aee3',
+                       'warn': '#ed9d20',
+                       'success': '#4cb24a',
+                       'color': ''
+                       }
+
+        self.open_tags = {'[system]': '<span style = "color:%s;">' % self.styles['system'],
+                          '[error]': '<span style = "color:%s;">' % self.styles['error'],
+                          '[info]': '<span style = "color:%s;">' % self.styles['info'],
+                          '[warn]': '<span style = "color:%s;">' % self.styles['warn'],
+                          '[success]': '<span style = "color:%s;">' % self.styles['success'],
+                          '[none]': '<span style = "color:%s;">' % self.styles['color'],
+                          '[b]': '<span style = "font-weight:bold;">',
+                          '[normal]': '<span style = "font-weight:normal;">',
+                          '[upper]': '<span style = "text-transform: uppercase;">',
+                          '[tab]': '&nbsp;&nbsp;&nbsp;&nbsp;'
+                          }
+
+        self.close_tags = {'[/system]': '</span>',
+                           '[/error]': '</span>',
+                           '[/info]': '</span>',
+                           '[/warn]': '</span>',
+                           '[/success]': '</span>',
+                           '[/none]': '</span>',
+                           '[/b]': '</span>',
+                           '[/normal]': '</span>',
+                           '[/upper]': '</span>',
+                           '[/tab]': '</span>'
+                           }
